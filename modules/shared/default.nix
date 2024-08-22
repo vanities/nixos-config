f8daec2de0bd66fe7c14b6eb5cf6f07fa8359c9f
Adam Mischke <mischkeaa@gmail.com>
2024-08-22 13:25:55 -0500

    remove templates

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  emacsOverlaySha256 = "06413w510jmld20i4lik9b36cfafm501864yq8k4vxl5r4hn0j0h";
+  emacsOverlaySha256 = "";
 in
 {
 
@@ -14,18 +14,4 @@ in
       allowInsecure = false;
       allowUnsupportedSystem = true;
     };
-
-    overlays =
-      # Apply each overlay found in the /overlays directory
-      let path = ../../overlays; in with builtins;
-      map (n: import (path + ("/" + n)))
-          (filter (n: match ".*\\.nix" n != null ||
-                      pathExists (path + ("/" + n + "/default.nix")))
-                  (attrNames (readDir path)))
-
-      ++ [(import (builtins.fetchTarball {
-               url = "https://github.com/dustinlyons/emacs-overlay/archive/refs/heads/master.tar.gz";
-               sha256 = emacsOverlaySha256;
-           }))];
-  };
 }
